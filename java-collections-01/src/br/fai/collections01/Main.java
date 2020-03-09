34b300c7a5912bc8d6410ee2b91a9b6d36dee497
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:24:04 -0300

    primeira alteracao

---
+++ java-collections-01/src/br/fai/collections01/Main.java
@@ -0,0 +1,21 @@
+package br.fai.collections01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+
+	}
+	
+	private Scanner scanner = new Scanner(System.in);
+
+	private void start() {
+		
+		int variavelLocal = 0;
+		
+	}
+}
